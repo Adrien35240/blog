2edf97f8195b1ff7fdd62023f1847f31eaa18c92
adrien35240 <adriendev35240@gmail.com>
2021-03-19 22:10:29 +0100

    ajout upload&read image firebase

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import { useHistory } from "react-router-dom";
-import { Grid, Typography, Button } from "@material-ui/core";
+import { Grid, Typography, Button, Input } from "@material-ui/core";
 import Alert from "@material-ui/lab/Alert";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import firebase from "firebase";
+import { storage } from "../firebase";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 export default function Dashboard() {
@@ -15,6 +16,8 @@ export default function Dashboard() {
   const [error, setError] = useState("");
   const { currentUser, logout } = useAuth();
   const history = useHistory();
+  const allInputs = { imgUrl: "" };
+  const [imageAsFile, setImageAsFile] = useState("");
 
   const useStyles = makeStyles((theme) => ({
     input: {
@@ -36,20 +39,41 @@ export default function Dashboard() {
     }
   }
   function handleChange(e) {
-    setContent(e)
+    setContent(e);
+    console.log(e);
   }
 
+  console.log(imageAsFile);
+  const handleImageAsFile = (e) => {
+    const image = e.target.files[0];
+    setImageAsFile((imageFile) => image);
+  };
+  function pushImg(imageAsFile) {
+    storage.ref(`/images/${imageAsFile.name}`).put(imageAsFile);
+  }
+
+
   async function handleSubmit() {
+    console.log("start of upload");
+    // async magic goes here...
+    if (imageAsFile === "") {
+      console.error(`not an image, the image file is a ${typeof imageAsFile}`);
+    }
+    await pushImg(imageAsFile); 
+  
+
     const data = {
       title: title,
       description: description,
       content: content,
+      img: await storage.ref("images").child(imageAsFile.name).getDownloadURL(),
     };
 
     // Add a new document in collection "article" with ID 'title'
     const res = await firebase.firestore().collection("articles").add(data);
     console.log("Set: ", res);
   }
+
 
   return (
     <Grid container justify="center">
@@ -77,6 +101,7 @@ export default function Dashboard() {
           value={description}
           onChange={(e) => setDescription(e.target.value)}
         />{" "}
+        <Input type="file" onChange={handleImageAsFile} />
         <ReactQuill value={content} onChange={handleChange} />
         <Button
           color="primary"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,24 @@ import React, { useState, useEffect } from "react";
 import LittleCard from "../components/LittleCard";
 import Grid from "@material-ui/core/Grid";
 import firebase from "firebase";
-import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    height: 100,
-  },
-}));
 function Home() {
   const [articles, setArticles] = useState([]);
-  const classes = useStyles();
+  const [urlImg, setUrlImg] = useState();
+
+  //  function getImage(nameImg) {
+  //    firebase
+  //     .storage()
+  //     .ref("images")
+  //     .child(nameImg)
+  //     .getDownloadURL()
+  //     .then((getUrl) => {
+  //       setUrlImg(getUrl);
+  //     });
+   
+  // }
+
+  //NOTE: Recupere le lien d'une image sur firebase
 
   useEffect(() => {
     firebase
@@ -25,27 +30,28 @@ function Home() {
         let documents = [];
         article.forEach((doc) => {
           documents.push({ ...doc.data(), id: doc.id });
-         
         });
         setArticles(documents);
-       
       });
   }, []);
 
   function renderArticles() {
     return (
-      <Grid container justify='center' spacing={2}>
+      <Grid container justify="center" spacing={2}>
         {articles &&
-          articles.map((article, index) => (
-            <Grid key={index} item xs={12} sm={3}>
-              <LittleCard
-                title={article.title}
-                description={article.description}
-                img="/empty-card.jpeg"
-                slug={article.title}
-              />
-            </Grid>
-          ))}
+          articles.map((article, index) => {
+           {/* getImage(article.img) */}
+            return (
+              <Grid key={index} item xs={12} sm={3}>
+                <LittleCard
+                  title={article.title}
+                  description={article.description}
+                  img={article.img}
+                  slug={article.title}
+                />
+              </Grid>
+            );
+          })}
       </Grid>
     );
   }
