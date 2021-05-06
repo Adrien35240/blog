aed8ca840a7cfe36f571cf3195929b3ff6619961
adrien35240 <ribaultadrien@orange.fr>
2021-05-06 17:57:07 +0200

    setup du fichier .env

--- src/services/database/firebase.js
+++ src/services/database/firebase.js
@@ -1,17 +1,19 @@
-import firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/storage"
+
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/storage";
+const dotenv = require("dotenv");
 const app = firebase.initializeApp({
-  //apiKey: process.env.REACT_APP_API_KEY,
-  apiKey: "AIzaSyA5L77NEcwnqKrAvxbEzQmQOK8gE_T1iJM",
-  authDomain: "mon-blog-e0a33.firebaseapp.com",
-  projectId: "mon-blog-e0a33",
-  storageBucket: "mon-blog-e0a33.appspot.com",
-  messagingSenderId: "936753106550",
-  appId: "1:936753106550:web:dde06226b58f33278dd957",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 // export const storage = firebase.storage();
 const storage = firebase.storage();
-export const auth = app.auth()
+export const auth = app.auth();
 // export default app
-export { storage,app as default };
+export { storage, app as default };
