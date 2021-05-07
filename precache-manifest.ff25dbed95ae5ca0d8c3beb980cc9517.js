b8957fef357ee6e511869c4958b2ab01950fbe5f
adrien35240 <ribaultadrien@orange.fr>
2021-05-07 19:12:53 +0200

    Updates

---
+++ precache-manifest.ff25dbed95ae5ca0d8c3beb980cc9517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cdc94b2ab5a74335fb822ed57b98a7",
+    "url": "/Blog/index.html"
+  },
+  {
+    "revision": "07dd1ff7b61b2dd865fb",
+    "url": "/Blog/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "2a6d4dcfe8f86574b229",
+    "url": "/Blog/static/css/main.9b8de9fb.chunk.css"
+  },
+  {
+    "revision": "07dd1ff7b61b2dd865fb",
+    "url": "/Blog/static/js/2.f75db854.chunk.js"
+  },
+  {
+    "revision": "9d14d9350ffdb8132e7ebe92e3b039c5",
+    "url": "/Blog/static/js/2.f75db854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a6d4dcfe8f86574b229",
+    "url": "/Blog/static/js/main.95aab381.chunk.js"
+  },
+  {
+    "revision": "e22c04c3e0ae6f6e6858",
+    "url": "/Blog/static/js/runtime-main.7264949f.js"
+  }
+]);
