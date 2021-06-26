827c14ab2a307ed381986b9a72ea6cced79e3f46
Adrien35240 <adriendev35240@gmail.com>
2021-06-26 17:02:06 +0200

    feat:code-block highlight

--- src/views/FocusPost/BigCard/Bigcard.js
+++ src/views/FocusPost/BigCard/Bigcard.js
@@ -1,17 +1,22 @@
 import React from "react";
 import "./bigcard.css";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 export default function BigCard(props) {
   function formatContent() {
     const text = props.content;
-    return (
-      <div component={"span"} dangerouslySetInnerHTML={{ __html: text }}></div>
-    );
+     return (
+       text
+     );
   }
   return (
     <div className="container-big-card">
       <img className="container-img" src={props.img} alt="no-img" />
       <div className="big-card-title">{props.title}</div>
-      <div className="big-card-content">{formatContent()}</div>
+      <div className="big-card-content">
+      <SyntaxHighlighter language="javascript" style={docco}>
+        {formatContent()}
+      </SyntaxHighlighter></div>
       {/* <Comments id={props.id} /> */}
     </div>
   );
